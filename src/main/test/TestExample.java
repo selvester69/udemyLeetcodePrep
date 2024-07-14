56daaa53b5d237025f5c120485bdc6150f8531c0
selvester69 <selvester69@gmail.com>
2024-07-14 17:43:54 +0530

    Setup completed

---
+++ src/main/test/TestExample.java
@@ -0,0 +1,13 @@
+package main.test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestExample {
+
+  @Test
+  public void test() {
+    assertEquals(true, true);
+  }
+
+}
