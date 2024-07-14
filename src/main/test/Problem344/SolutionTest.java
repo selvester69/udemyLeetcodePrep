da3638600c190591b99068a7fa9e6c5894ba728e
selvester69 <selvester69@gmail.com>
2024-07-14 21:15:40 +0530

    Solution to Problem-344 with test cases

---
+++ src/main/test/Problem344/SolutionTest.java
@@ -0,0 +1,92 @@
+package main.test.Problem344;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+import main.java.Problem344.Solution344;
+
+public class SolutionTest {
+    Solution344 solution = new Solution344();
+
+    @Test
+    public void testMultipleCharacters() {
+
+        char[] input = { 'h', 'e', 'l', 'l', 'o' };
+        char[] expected = { 'o', 'l', 'l', 'e', 'h' };
+        solution.reverseString(input);
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testSingleCharacter() {
+
+        char[] input = { 'a' };
+        char[] expected = { 'a' };
+        solution.reverseString(input);
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testEvenNumberOfCharacters() {
+
+        char[] input = { 'A', 'B', 'C', 'D' };
+        char[] expected = { 'D', 'C', 'B', 'A' };
+        solution.reverseString(input);
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testOddNumberOfCharacters() {
+
+        char[] input = { '1', '2', '3', '4', '5' };
+        char[] expected = { '5', '4', '3', '2', '1' };
+        solution.reverseString(input);
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testCharactersWithSpaces() {
+
+        char[] input = { ' ', 'a', ' ', 'b', ' ' };
+        char[] expected = { ' ', 'b', ' ', 'a', ' ' };
+        solution.reverseString(input);
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testEmptyArray() {
+
+        char[] input = {};
+        char[] expected = {};
+        solution.reverseString(input);
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testPalindromicString() {
+
+        char[] input = { 'r', 'a', 'c', 'e', 'c', 'a', 'r' };
+        char[] expected = { 'r', 'a', 'c', 'e', 'c', 'a', 'r' };
+        solution.reverseString(input);
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testNumbersAndLetters() {
+
+        char[] input = { '1', 'a', '2', 'b', '3' };
+        char[] expected = { '3', 'b', '2', 'a', '1' };
+        solution.reverseString(input);
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testSpecialCharacters() {
+
+        char[] input = { '!', '@', '#', '$', '%' };
+        char[] expected = { '%', '$', '#', '@', '!' };
+        solution.reverseString(input);
+        assertArrayEquals(expected, input);
+    }
+}
