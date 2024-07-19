6fb9ab60f6667e263bc6a4a97da346f05927c6f3
selvester69 <selvester69@gmail.com>
2024-07-19 23:03:27 +0530

    Problem-1047 | remove adjacent duplicates-1

---
+++ src/main/java/Easy/Problem1047/Solution.java
@@ -0,0 +1,83 @@
+package Easy.Problem1047;
+
+import java.util.Stack;
+
+/**
+ * You are given a string s consisting of lowercase English letters. A duplicate
+ * removal consists of choosing two adjacent and equal letters and removing
+ * them.
+ * 
+ * We repeatedly make duplicate removals on s until we no longer can.
+ * 
+ * Return the final string after all such duplicate removals have been made. It
+ * can be proven that the answer is unique.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: s = "abbaca"
+ * Output: "ca"
+ * Explanation:
+ * For example, in "abbaca" we could remove "bb" since the letters are adjacent
+ * and equal, and this is the only possible move. The result of this move is
+ * that the string is "aaca", of which only "aa" is possible, so the final
+ * string is "ca".
+ * Example 2:
+ * 
+ * Input: s = "azxxzy"
+ * Output: "ay"
+ * 
+ * 
+ * Constraints:
+ * 
+ * 1 <= s.length <= 105
+ * s consists of lowercase English letters.
+ */
+
+public class Solution {
+
+    // ====> Method 1 <====
+    public String removeDuplicates_recursion(String s) {
+        String s1 = "";
+        while (s1.length() != s.length()) {
+            s1 = s;
+            s = check(s);
+        }
+        return s;
+    }
+
+    public String check(String s) {
+        String ans = "";
+        int i = 0;
+        int n = s.length();
+        while (i < n) {
+            if (i < n - 1 && s.charAt(i) == s.charAt(i + 1)) {
+                i++;
+                // while (i < n - 1 && s.charAt(i) == s.charAt(i + 1))
+            } else {
+                ans += s.charAt(i);
+            }
+            i++;
+        }
+        return ans;
+    }
+
+    // ====> Method 2 using stack <====
+    public String removeDuplicates_stack(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (stack.isEmpty() || stack.peek() != s.charAt(i)) {
+                stack.push(s.charAt(i));
+            } else {
+                stack.pop();
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+
+}
---
+++ src/test/java/SolutionsTest/Problem1047.java
@@ -0,0 +1,206 @@
+package SolutionsTest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import Easy.Problem1047.Solution;
+
+public class Problem1047 {
+
+    Solution solution = new Solution();
+
+    @Test
+    public void testEmptyString_usingRecursion() {
+
+        String input = "";
+        String expected = "";
+        assertEquals(expected, solution.removeDuplicates_recursion(input));
+    }
+
+    @Test
+    public void testNoDuplicates_usingRecursion() {
+
+        String input = "abc";
+        String expected = "abc";
+        assertEquals(expected, solution.removeDuplicates_recursion(input));
+    }
+
+    @Test
+    public void testSingleCharacter_usingRecursion() {
+
+        String input = "a";
+        String expected = "a";
+        assertEquals(expected, solution.removeDuplicates_recursion(input));
+    }
+
+    @Test
+    public void testAdjacentDuplicates_usingRecursion() {
+
+        String input = "abbaca";
+        String expected = "ca";
+        assertEquals(expected, solution.removeDuplicates_recursion(input));
+    }
+
+    @Test
+    public void testMultipleAdjacentDuplicates_usingRecursion() {
+
+        String input = "azxxzy";
+        String expected = "ay";
+        assertEquals(expected, solution.removeDuplicates_recursion(input));
+    }
+
+    @Test
+    public void testAllCharactersAreDuplicates_usingRecursion() {
+
+        String input = "aaaa";
+        String expected = "";
+        assertEquals(expected, solution.removeDuplicates_recursion(input));
+    }
+
+    @Test
+    public void testComplexCase1_usingRecursion() {
+
+        String input = "abbcccbba";
+        String expected = "";
+        assertEquals(expected, solution.removeDuplicates_recursion(input));
+    }
+
+    @Test
+    public void testComplexCase2_usingRecursion() {
+
+        String input = "abccba";
+        String expected = "";
+        assertEquals(expected, solution.removeDuplicates_recursion(input));
+    }
+
+    @Test
+    public void testSinglePairOfAdjacentDuplicates_usingRecursion() {
+
+        String input = "aabb";
+        String expected = "";
+        assertEquals(expected, solution.removeDuplicates_recursion(input));
+    }
+
+    @Test
+    public void testStringWithNoRemovals_usingRecursion() {
+
+        String input = "abcdefg";
+        String expected = "abcdefg";
+        assertEquals(expected, solution.removeDuplicates_recursion(input));
+    }
+
+    @Test
+    public void testStringEndsWithDuplicates_usingRecursion() {
+
+        String input = "abcddee";
+        String expected = "abc";
+        assertEquals(expected, solution.removeDuplicates_recursion(input));
+    }
+
+    @Test
+    public void testStringStartsWithDuplicates_usingRecursion() {
+
+        String input = "aabccba";
+        String expected = "a";
+        assertEquals(expected, solution.removeDuplicates_recursion(input));
+    }
+
+    // ==============================> Using stack <=======================
+
+    @Test
+    public void testEmptyString() {
+
+        String input = "";
+        String expected = "";
+        assertEquals(expected, solution.removeDuplicates_stack(input));
+    }
+
+    @Test
+    public void testNoDuplicates() {
+
+        String input = "abc";
+        String expected = "abc";
+        assertEquals(expected, solution.removeDuplicates_stack(input));
+    }
+
+    @Test
+    public void testSingleCharacter() {
+
+        String input = "a";
+        String expected = "a";
+        assertEquals(expected, solution.removeDuplicates_stack(input));
+    }
+
+    @Test
+    public void testAdjacentDuplicates() {
+
+        String input = "abbaca";
+        String expected = "ca";
+        assertEquals(expected, solution.removeDuplicates_stack(input));
+    }
+
+    @Test
+    public void testMultipleAdjacentDuplicates() {
+
+        String input = "azxxzy";
+        String expected = "ay";
+        assertEquals(expected, solution.removeDuplicates_stack(input));
+    }
+
+    @Test
+    public void testAllCharactersAreDuplicates() {
+
+        String input = "aaaaaaaaa";
+        String expected = "a";
+        assertEquals(expected, solution.removeDuplicates_stack(input));
+    }
+
+    @Test
+    public void testComplexCase1() {
+
+        String input = "abbcccbba";
+        String expected = "aca";
+        assertEquals(expected, solution.removeDuplicates_stack(input));
+    }
+
+    @Test
+    public void testComplexCase2() {
+
+        String input = "abccba";
+        String expected = "";
+        assertEquals(expected, solution.removeDuplicates_stack(input));
+    }
+
+    @Test
+    public void testSinglePairOfAdjacentDuplicates() {
+
+        String input = "aabb";
+        String expected = "";
+        assertEquals(expected, solution.removeDuplicates_stack(input));
+    }
+
+    @Test
+    public void testStringWithNoRemovals() {
+
+        String input = "abcdefg";
+        String expected = "abcdefg";
+        assertEquals(expected, solution.removeDuplicates_stack(input));
+    }
+
+    @Test
+    public void testStringEndsWithDuplicates() {
+
+        String input = "abcddee";
+        String expected = "abc";
+        assertEquals(expected, solution.removeDuplicates_stack(input));
+    }
+
+    @Test
+    public void testStringStartsWithDuplicates() {
+
+        String input = "aabccba";
+        String expected = "a";
+        assertEquals(expected, solution.removeDuplicates_stack(input));
+    }
+
+}
