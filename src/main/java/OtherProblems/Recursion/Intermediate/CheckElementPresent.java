dfa6e7124aa9b1fd61756ded8294b35bcf39e1c9
selvester69 <selvester69@gmail.com>
2024-07-22 22:57:17 +0530

    Problem - recursion - intermediate - check if element is present in array

--- src/main/java/OtherProblems/Recursion/Intermediate/CheckElementPresent.java
+++ src/main/java/OtherProblems/Recursion/Intermediate/CheckElementPresent.java
@@ -3,7 +3,17 @@ package OtherProblems.Recursion.Intermediate;
 public class CheckElementPresent {
 
     public boolean isElementPresent(int[] arr, int x) {
-        return false;
+        if (arr.length == 0)
+            return false;
+        return checkElementRecursive(arr, x, arr.length, 0);
+    }
+
+    public boolean checkElementRecursive(int[] arr, int x, int n, int i) {
+        if (i == n)
+            return false;
+        if (arr[i] == x)
+            return true;
+        return checkElementRecursive(arr, x, n, i + 1);
     }
 
 }
