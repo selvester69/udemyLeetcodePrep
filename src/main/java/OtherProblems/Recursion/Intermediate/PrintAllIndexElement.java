a525687e5fd2db732de63ea0928aca2a718ca18a
selvester69 <selvester69@gmail.com>
2024-07-22 22:59:35 +0530

    Problem - recursion - intermediate - return all index of given element in an array

--- src/main/java/OtherProblems/Recursion/Intermediate/PrintAllIndexElement.java
+++ src/main/java/OtherProblems/Recursion/Intermediate/PrintAllIndexElement.java
@@ -1,11 +1,22 @@
 package OtherProblems.Recursion.Intermediate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrintAllIndexElement {
 
     public List<Integer> findAllPositions(int[] arr, int x) {
-        return null;
+        List<Integer> position = new ArrayList<>();
+        findAllIndexRecursive(arr, x, arr.length, 0, position);
+        return position;
+    }
+
+    public void findAllIndexRecursive(int[] arr, int x, int n, int i, List<Integer> position) {
+        if (i == n)
+            return;
+        if (arr[i] == x)
+            position.add(i);
+        findAllIndexRecursive(arr, x, n, i + 1, position);
     }
 
 }
