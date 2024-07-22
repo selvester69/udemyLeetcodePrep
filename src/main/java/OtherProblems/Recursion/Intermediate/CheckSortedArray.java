778a38749947edaf16e41f7373ed3e2d6d6dadc9
selvester69 <selvester69@gmail.com>
2024-07-22 22:57:51 +0530

    Problem - recursion - intermediate - check if given array is sorted

--- src/main/java/OtherProblems/Recursion/Intermediate/CheckSortedArray.java
+++ src/main/java/OtherProblems/Recursion/Intermediate/CheckSortedArray.java
@@ -3,8 +3,21 @@ package OtherProblems.Recursion.Intermediate;
 public class CheckSortedArray {
 
     public boolean isArraySorted(int arr[]) {
+        // edge case
+        if (arr.length == 0 || arr.length == 1)
+            return true;
+        return isSorted(arr, arr.length, 1);
+    }
 
-        return false;
+    public boolean isSorted(int arr[], int n, int i) {
+        // base case
+        if (i == n)
+            return true;
+        // recursive case
+        if (arr[i - 1] < arr[i])
+            return isSorted(arr, n, i + 1);
+        else
+            return false;
     }
 
 }
