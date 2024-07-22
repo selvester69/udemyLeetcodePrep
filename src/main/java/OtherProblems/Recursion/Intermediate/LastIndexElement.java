e552e4838270db4940aaecc6370373f753f794a7
selvester69 <selvester69@gmail.com>
2024-07-22 22:59:03 +0530

    Problem - recursion - intermediate - find last index of given element in an array

--- src/main/java/OtherProblems/Recursion/Intermediate/LastIndexElement.java
+++ src/main/java/OtherProblems/Recursion/Intermediate/LastIndexElement.java
@@ -3,7 +3,19 @@ package OtherProblems.Recursion.Intermediate;
 public class LastIndexElement {
 
     public int findLastIndex(int[] arr, int x) {
-        return -1;
+        if (arr.length == 0)
+            return -1;
+        return lastIndexRecursive(arr, x, arr.length, arr.length - 1);
+
+    }
+
+    public int lastIndexRecursive(int[] arr, int x, int n, int i) {
+        if (i == -1)
+            return -1;
+        if (arr[i] == x)
+            return i;
+
+        return lastIndexRecursive(arr, x, n, i - 1);
 
     }
 
