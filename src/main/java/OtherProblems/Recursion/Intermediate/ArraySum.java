c3df1d2bf473fbdcc35c66f31209626170dd546c
selvester69 <selvester69@gmail.com>
2024-07-22 22:56:54 +0530

    Problem - recursion - intermediate -  arraySum

--- src/main/java/OtherProblems/Recursion/Intermediate/ArraySum.java
+++ src/main/java/OtherProblems/Recursion/Intermediate/ArraySum.java
@@ -3,7 +3,19 @@ package OtherProblems.Recursion.Intermediate;
 public class ArraySum {
 
     public int sumArray(int[] arr) {
-        return -1;
+        if (arr.length == 0)
+            return 0;
+        if (arr.length == 1)
+            return arr[0];
+        return isSumRecursive(arr, arr.length, 0);
+    }
+
+    public int isSumRecursive(int arr[], int n, int i) {
+        if (i == n)
+            return 0;
+
+        return arr[i] + isSumRecursive(arr, n, i + 1);
+
     }
 
 }
