1b6c6bd9aca71ee0a4eff6604617fa3f04159f79
selvester69 <selvester69@gmail.com>
2024-07-22 22:58:20 +0530

    Problem - recursion - intermediate - count all the occurrence of given element in an array

--- src/main/java/OtherProblems/Recursion/Intermediate/CountOccurrence.java
+++ src/main/java/OtherProblems/Recursion/Intermediate/CountOccurrence.java
@@ -3,7 +3,19 @@ package OtherProblems.Recursion.Intermediate;
 public class CountOccurrence {
 
     public int countOccurrences(int[] arr, int x) {
-        return -1;
+        if (arr.length == 0)
+            return 0;
+
+        return countRecursive(arr, x, arr.length, 0);
+    }
+
+    public int countRecursive(int[] arr, int x, int n, int i) {
+        if (i == n)
+            return 0;
+        if (arr[i] == x)
+            return 1 + countRecursive(arr, x, n, i + 1);
+        return countRecursive(arr, x, n, i + 1);
+
     }
 
 }
