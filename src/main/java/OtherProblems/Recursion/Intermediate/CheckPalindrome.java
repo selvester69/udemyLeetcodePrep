02fb6e79fcd754f0d78069b13d8df33576ae6d81
selvester69 <selvester69@gmail.com>
2024-07-22 22:57:35 +0530

    Problem - recursion - intermediate - check if given array is palindrome

--- src/main/java/OtherProblems/Recursion/Intermediate/CheckPalindrome.java
+++ src/main/java/OtherProblems/Recursion/Intermediate/CheckPalindrome.java
@@ -3,6 +3,18 @@ package OtherProblems.Recursion.Intermediate;
 public class CheckPalindrome {
 
     public boolean isPalindrome(int[] arr) {
+        if (arr.length == 0 || arr.length == 1)
+            return true;
+        return isResursive(arr, 0, arr.length - 1);
+    }
+
+    public boolean isResursive(int[] arr, int start, int end) {
+        if (start > end)
+            return true;
+
+        if (arr[start] == arr[end]) {
+            return isResursive(arr, start + 1, end - 1);
+        }
         return false;
     }
 
