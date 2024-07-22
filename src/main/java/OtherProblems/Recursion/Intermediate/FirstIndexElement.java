f09b9039180f837b4a073110ad96903c639ebfbe
selvester69 <selvester69@gmail.com>
2024-07-22 22:58:45 +0530

    Problem - recursion - intermediate - find first of given element in an array

--- src/main/java/OtherProblems/Recursion/Intermediate/FirstIndexElement.java
+++ src/main/java/OtherProblems/Recursion/Intermediate/FirstIndexElement.java
@@ -3,7 +3,16 @@ package OtherProblems.Recursion.Intermediate;
 public class FirstIndexElement {
 
     public int findFirstIndex(int[] arr, int x) {
-        return -1;
+        if (arr.length == 0)
+            return -1;
+        return firstIndexRecursive(arr, x, arr.length, 0);
     }
 
+    public int firstIndexRecursive(int[] arr, int x, int n, int i) {
+        if (i == n)
+            return -1;
+        if (arr[i] == x)
+            return i;
+        return firstIndexRecursive(arr, x, n, i + 1);
+    }
 }
